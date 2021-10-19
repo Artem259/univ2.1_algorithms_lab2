38cb7bd87d23de14b3d7db24ff93e3bc1f801248
Artem Poliakov <artem4250@gmail.com>
2021-10-19 13:20:00 +0300

    update

--- StringAlgorithm.cpp
+++ StringAlgorithm.cpp
@@ -3,16 +3,15 @@
 //
 
 #include "StringAlgorithm.h"
-#include <iostream>
 
+//Перший етап алгоритму. Допоміжна префікс-функція, яка формує масив pi для вхідного рядка s
 std::vector<size_t> PrefixFunction(const std::string& s)
 {
-    size_t len = s.length();
-    std::vector<size_t> pi(len, 0);
-    size_t j = 0;
-    for(size_t i=1; i<len; i++)
+    size_t len = s.length(); //обчислення довжини вхідного рядка s
+    std::vector<size_t> pi(len, 0); //створення результуючого масиву тієї ж довжини
+    size_t j = 0; //вказівник у рядку
+    for(size_t i=1; i<len; i++) //прохід рядком, обчислення значень елементів результуючого масиву pi
     {
-        //j = pi[i-1];
         while(j!=0 && s[i]!=s[j]) j = pi[j-1];
         if(s[i] == s[j]) j++;
         pi[i] = j;
@@ -20,16 +19,17 @@ std::vector<size_t> PrefixFunction(const std::string& s)
     return pi;
 }
 
+//Другий етап алгоритму. Основна функція, в якій реалізована перевірка циклічності вхідних рядків s1 та s2
 bool CyclicShift(const std::string& s1, const std::string& s2)
 {
     size_t len = s1.length();
     size_t k = 0; //вказівник у основному рядку s1
     size_t l = 0; //вказівник у "підрядку" s2
-    if(s2.length()!=len) return false;
-    if(len==0) return true;
-    auto pi = PrefixFunction(s2);
-    bool cycle = false;
-    while(true)
+    if(s2.length()!=len) return false; //якщо довжини вхідних рядків не рівні - рядки не циклічні
+    if(len==0) return true; //якщо рядки порожні - повертається true
+    auto pi = PrefixFunction(s2); //формування префікс-масиву для рядка s2
+    bool cycle = false; //перший чи других прохід рядком s1?
+    while(true) //порівняння рядків
     {
         if(s1[k]==s2[l])
         {
@@ -42,10 +42,10 @@ bool CyclicShift(const std::string& s1, const std::string& s2)
             else l = pi[l-1];
         }
 
-        if(l==len) return true;
+        if(l==len) return true; //рядки є циклічними
         if(k==len)
         {
-            if(cycle) return false;
+            if(cycle) return false; //рядки не є циклічними
             cycle = true;
             k=0;
         }
